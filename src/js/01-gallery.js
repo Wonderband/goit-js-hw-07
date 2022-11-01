0577047c7d8f70857b82ad85f49f262bafed9bc0
Alex Koniushenko <alex.koniushenko@gmail.com>
2022-11-02 00:35:06 +0200

    1st task done - listeners to remove

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const modalWindowCloser = (event, mv) => {
+    if (event.code === "Escape") {
+        mv.close(); 
+        document.removeEventListener("keydown",  ev => modalWindowCloser(ev, modalWindow));
+        console.log('removed');
+    }  
+}
 
-console.log(galleryItems);
+const modalWindowOpener = event => {
+    event.preventDefault();
+    if (event.target.tagName !== "IMG")
+        return;
+    const zoomPictureUrl = event.target.dataset.source;  
+    const modalWindow = basicLightbox.create(`
+    <div class="modal">
+        <img src="${zoomPictureUrl}" width="800">
+    </div>`);
+    modalWindow.show();   
+    document.addEventListener("keydown", ev => modalWindowCloser(ev, modalWindow));  
+}
+
+let markUp = '';
+for (const galleryItem of galleryItems) {
+    markUp += `<div class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+    <img class="gallery__image"src="${galleryItem.preview}" 
+    data-source="${galleryItem.original}" 
+    alt="Image description"/></a>
+    </div>`;    
+}
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = markUp;
+galleryContainer.addEventListener('click', event => modalWindowOpener(event));
+    
+
+
